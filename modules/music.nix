36725f7579aec92432213a0d4966504d77efb757
Vivien Lelouette <lelouette.vivien@gmail.com>
2022-09-20 19:30:47 +0200

    raise ulimit for ardour

--- modules/music.nix
+++ modules/music.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 {
+  systemd.services."user@1000".serviceConfig.LimitNOFILE = "32768";
+  security.pam.loginLimits = [
+    { domain = "*"; item = "nofile"; type = "-"; value = "32768"; }
+    { domain = "*"; item = "memlock"; type = "-"; value = "32768"; }
+  ];
+  
   users.users.vivien = {
-
     packages = with pkgs; [
       ardour
       guitarix
@@ -9,7 +14,6 @@
       yabridge
       yabridgectl
     ];
-
   };
 
   environment.systemPackages = with pkgs; [
