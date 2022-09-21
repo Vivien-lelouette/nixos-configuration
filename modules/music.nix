322503ba024a7da35846f477540f66df98b7e9cc
Vivien Lelouette <lelouette.vivien@gmail.com>
2022-09-21 14:57:59 +0200

    Improve music setup

--- modules/music.nix
+++ modules/music.nix
@@ -3,7 +3,6 @@
   users.users.vivien = {
     packages = with pkgs; [
       ardour
-      guitarix
       wineWowPackages.stable
       yabridge
       yabridgectl
@@ -12,11 +11,18 @@
   };
 
   environment.systemPackages = with pkgs; [
+    a2jmidid
+    guitarix
     gxplugins-lv2
+    lsp-plugins
+    surge-XT
+    cardinal
+    mda_lv2
+    x42-plugins
   ];
   environment.variables.LV2_PATH = "/run/current-system/sw/lib/lv2";
 
   security.pam.loginLimits = [
     { domain = "*"; item = "memlock"; type = "-"; value = "unlimited"; }
   ];
-} 
+}
